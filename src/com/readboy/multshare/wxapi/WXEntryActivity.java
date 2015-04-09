7837927bc8f90f322ab39363be59f8ee66bcac22
iamBond <oxfff_cs@163.com>
2015-04-09 17:21:33 +0800

    first commit

---
+++ src/com/readboy/multshare/wxapi/WXEntryActivity.java
@@ -0,0 +1,36 @@
+
+package com.readboy.multshare.wxapi;
+
+import android.os.Bundle;
+
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+	@Override
+	protected void initWXHandler() {
+		// TODO Auto-generated method stub
+		super.initWXHandler();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onReq(BaseReq req) {
+		// TODO Auto-generated method stub
+		super.onReq(req);
+	}
+
+	@Override
+	public void onResp(BaseResp resp) {
+		// TODO Auto-generated method stub
+		super.onResp(resp);
+	}
+
+}
